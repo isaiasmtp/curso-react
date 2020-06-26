c2722fcbfd52d088a3814f76f34dcc02d63972ec
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2020-06-26 17:42:10 -0300

    Adding Frontend

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -7,7 +7,7 @@ const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended:true }))
 server.use(bodyParser.json())
-// server.use(allowCors)
+server.use(allowCors)
 
 server.listen(port, function (){
     console.log(`BACKEND is running on port ${port}`);    
