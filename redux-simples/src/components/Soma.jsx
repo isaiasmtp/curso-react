ec1c13781f5b9a67ed8b861cf038e0cda1c3fe7e
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2020-07-02 17:34:41 -0300

    Adicionando Action Creator

--- redux-simples/src/components/Soma.jsx
+++ redux-simples/src/components/Soma.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import Card from './Card'
 
-export default props => {
+import { connect } from 'react-redux'
+
+
+const Soma = props => {
+    const { min, max } = props
     return (
         <Card title="Soma dos Números" blue>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>10</strong>
+                    <strong>{max + min}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Soma)
---
+++ redux-simples/src/store/storeConfig.js
@@ -0,0 +1,32 @@
+import { createStore, combineReducers} from 'redux'
+
+const reducers = combineReducers({
+  numeros: function(state, action) {   
+
+    switch(action.type){
+      case 'NUM_MIN_ALTERADO':
+        return{
+          ...state,
+          min: action.payload
+        }
+      
+      case 'NUM_MAX_ALTERADO':
+      return{
+        ...state,
+        max: action.payload
+      } 
+
+      default:
+        return{
+          min: 7,
+          max: 31
+      }
+    }
+  },
+})
+
+function storeConfig(){
+    return createStore(reducers)
+}
+
+export default storeConfig
